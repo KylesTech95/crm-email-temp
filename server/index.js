3a130358d0dc09571d2ef3a7461c2a164a2068eb
kylestech95 <kylestech95@penguin>
2024-08-17 08:03:37 -0400

    feat: fastify implementation

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-const fastify = require('fastify')({logger:true})
-const PORT = {port:3223}// fastify - port = object 
+const fastify = require("fastify")({ logger: false });
+const path = require("path");
 
+const middleware = async (fast) => {
+    fast.register(require("@fastify/express"));
+    fast.register(require("@fastify/static"),{
+        root:path.join(__dirname,'/../public'),
+        constraints:{},
+    });
+};
+const PORT = { port: 3223 }; // fastify - port = object
+middleware(fastify);
 
+fastify.get("/hello", (request, reply) => {
+  const template = "<h1>Hello Fastify</h1>";
+  reply.type("text/html").code(200);
+  return template;
+});
 
-fastify.get('/',(request,reply)=>{
-    const template = '<h1>Hello Fastify</h1>'
-    reply.type('text/html').code(200)
-    return template;
-})
-
-
-fastify.listen(PORT,(err,address)=>{
-    return err ? console.log(err) : console.log('Listening on port '+address)
-})
+fastify.listen(PORT, (err, address) => {
+  return err ? console.log(err) : console.log("Listening on port " + address);
+});
