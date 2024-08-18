d0d17ab51821a87480ea591d293d21bd8a6d0db4
kylestech95 <kylestech95@penguin>
2024-08-18 18:01:25 -0400

    fastify - explanation

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 <div style="text-align:center;width:100%;padding:.5rem 0">
-<h1>Email templates</h1>
+<h1 >Fastify Setup</h1>
+</div>
+  <h3>Declare & instantiate a port to listen on</h3>
+<div
+  style="
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+  "
+>
+  <img src="./media/one.png" alt=""/>
+
+  <hr style="width:75%;border:none;background:#fff; margin:1rem auto;"/>
 </div>
 <div
   style="
@@ -10,9 +24,36 @@
     justify-content: center;
   "
 >
-  <h3>Birthday recognition</h3>
-  <img src="./public/media/birthday-card.png" alt=""/>
-  <a style="font-size:16px;margin-top:12px;text-decoration:none;" href="https://github.com/KylesTech95/crm-email-temp/tree/birthday">Link to Branch</a>
+  <h3>Setup Middleware</h3>
+  <img src="./media/two.png" alt=""/>
+
+  <hr style="width:75%;border:none;background:#fff; margin:1rem auto;"/>
+</div>
+<div
+  style="
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+  "
+>
+  <h3>Declare routes</h3>
+  <img src="./media/three.png" alt=""/>
+
+  <hr style="width:75%;border:none;background:#fff; margin:1rem auto;"/>
+</div>
+<div
+  style="
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+  "
+>
+  <h3>Listen on port</h3>
+  <img src="./media/four.png" alt=""/>
 
   <hr style="width:75%;border:none;background:#fff; margin:1rem auto;"/>
 </div>
--- server/index.js
+++ server/index.js
@@ -1,37 +1,49 @@
+// declare variables
 const fastify = require("fastify")({ logger: false });
 const path = require("path");
-const fs = require('fs')
-
-const middleware = async (fast) => {
-    fast.register(require("@fastify/express"));
-    fast.register(require("@fastify/static"),{
-        root:path.join(__dirname,'/../public'),
-        constraints:{},
-    });
-};
+const fs = require("fs");
+// port
 const PORT = { port: 3223 }; // fastify - port = object
+
+// middleware helper function
+const middleware = async (fast) => {
+  fast.register(require("@fastify/express"));
+  fast.register(require("@fastify/static"), {
+    root: path.join(__dirname, "/../public"),
+    constraints: {},
+  });
+};
+
+// middleware
 middleware(fastify);
 
+// get html
 fastify.get("/hello", (request, reply) => {
   const template = "<h1>Hello Fastify</h1>";
   reply.type("text/html").code(200);
   return template;
 });
 
-fastify.get('/reference',async (req,rep)=>{
+// get html via fs - readFileSync
+fastify.get("/reference", async (req, rep) => {
   // store reference html temp in memory with fs
-  let reference = path.resolve(__dirname,'..','public/reference.html')
-  const stream = fs.readFileSync(reference)
-  rep.type('text/html').send(stream)
-  })
+  let reference = path.resolve(__dirname, "..", "public/reference.html");
+  const stream = fs.readFileSync(reference);
+  rep.type("text/html").send(stream);
+});
 
-  fastify.get('/html-file',(req,rep)=>{
-    let readFile = fs.readFileSync(path.resolve(__dirname,'../public/index.html'),'utf-8')
-    let file = path.resolve(__dirname,'../public/index.html')
-    let data =  Buffer.from(readFile,'utf-8')
-    rep.type('text/html').send(data)
-  })
+// get html via Buffer.from
+fastify.get("/html-file", (req, rep) => {
+  let readFile = fs.readFileSync(
+    path.resolve(__dirname, "../public/index.html"),
+    "utf-8"
+  );
+  let file = path.resolve(__dirname, "../public/index.html");
+  let data = Buffer.from(readFile, "utf-8");
+  rep.type("text/html").send(data);
+});
 
+// listen on server
 fastify.listen(PORT, (err, address) => {
   return err ? console.log(err) : console.log("Listening on port " + address);
 });
